a49e159474b3931ed60e7fbc45e6c62c0534a9eb
Kiryl Shchasny <folkbread@gmail.com>
2023-10-25 10:57:34 +0200

    fix: styles for win with info about no available countries

--- countries-api/src/components/Cards/Cards.tsx
+++ countries-api/src/components/Cards/Cards.tsx
@@ -38,7 +38,7 @@ const Cards = ({
           />
         ))}
       {!isLoading && !isError && isFetched && visibleCountries.length === 0 && (
-        <Box className="bg-white dark:bg-elems-dark shadow h-40 w-full max-w-2xl flex justify-center items-center  text-2xl uppercase font-semibold text-slate-500 rounded-md">
+        <Box className="bg-white dark:bg-elems-dark shadow h-28 md:h-40 w-full flex justify-center items-center text-xl md:text-2xl uppercase font-semibold text-slate-500 rounded-md col-span-3 xl:col-start-2 xl:col-end-4 text-center">
           this country is not in the database
         </Box>
       )}
